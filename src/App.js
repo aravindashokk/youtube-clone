4a3656c2cc57d6de0a1eda5456047953c149c58b
aravindashokk <aravindkayal@gmail.com>
2025-01-24 21:37:13 -0600

    feat(ui): configured redux  and created appSlice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { Provider } from "react-redux";
 import Body from "./components/Body";
 import Head from "./components/Head";
+import store from "./utilities/store";
 
 
 function App() {
   return (
     <div className="">
-      <Head />
-      <Body />
+      <Provider store={store}>
+        <Head />
+        <Body />
+      </Provider>
     </div>
   );
 }
