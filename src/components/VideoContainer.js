a1f200b08726d7e86cf207430ce66a3f61f0e598
aravindashokk <aravindkayal@gmail.com>
2025-01-26 17:16:25 -0600

    feat(ui): add VideoContainer with VideoCards and Youtube API

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { YOUTUBE_VIDEOS_API } from '../utilities/constants';
+import VideoCard from './VideoCard';
 
 const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    setVideos(json.items)
+  }
+
   return (
-    <div>VideoContainer</div>
+    <div className='flex flex-wrap'>
+      {videos.map(video => <VideoCard key={video.id} info={video} />)}
+
+    </div>
   )
 };
 
--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,4 +1,6 @@
 export const HAMBURGER_ICON = "https://static.vecteezy.com/system/resources/previews/021/190/402/non_2x/hamburger-menu-filled-icon-in-transparent-background-basic-app-and-web-ui-bold-line-icon-eps10-free-vector.jpg";
 export const YOUTUBE_ICON = "https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo-2017-present.jpg";
 export const USER_ICON = "https://static.vecteezy.com/system/resources/previews/000/550/731/original/user-icon-vector.jpg";
-export const BUTTON_LIST_SUGGESTION = ["All", "Gaming", "Songs", "Live", "Cricket", "Chess", "News", "Cooking", "Podcasts", "Music", "History", "Mixes", "Football"]
+export const BUTTON_LIST_SUGGESTION = ["All", "Gaming", "Songs", "Live", "Cricket", "Chess", "News", "Cooking", "Podcasts", "Music", "History", "Mixes", "Football"];
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&maxResults=50&key="+process.env.REACT_APP_MY_GOOGLE_API_KEY;
+
