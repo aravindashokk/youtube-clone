6d3c0acdb6b16cace7c89cd82ef23fd91db8c390
aravindashokk <aravindkayal@gmail.com>
2025-01-26 15:26:24 -0600

    fix(ui): ButtonList component added with buttons and data from constants file.

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Button from './Button';
+import { BUTTON_LIST_SUGGESTION } from '../utilities/constants';
+
 
 const ButtonList = () => {
   return (
     <div className='flex m-2'>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      {BUTTON_LIST_SUGGESTION.map((item,index)=>
+        <Button key={index} name={item}/>
+      )}
     </div>
   )
 }
--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,3 +1,4 @@
 export const HAMBURGER_ICON = "https://static.vecteezy.com/system/resources/previews/021/190/402/non_2x/hamburger-menu-filled-icon-in-transparent-background-basic-app-and-web-ui-bold-line-icon-eps10-free-vector.jpg";
 export const YOUTUBE_ICON = "https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo-2017-present.jpg";
 export const USER_ICON = "https://static.vecteezy.com/system/resources/previews/000/550/731/original/user-icon-vector.jpg";
+export const BUTTON_LIST_SUGGESTION = ["All", "Gaming", "Songs", "Live", "Cricket", "Chess", "News", "Cooking", "Podcasts", "Music", "History", "Mixes", "Football"]
