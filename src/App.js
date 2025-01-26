cbf9eec057d13efde40198c772c2ec292de59d3a
aravindashokk <aravindkayal@gmail.com>
2025-01-26 17:28:09 -0600

    feat(ui): Setup routing and WatchPage layout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import { Provider } from "react-redux";
 import Body from "./components/Body";
 import Head from "./components/Head";
 import store from "./utilities/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
 
 
 function App() {
+  const appRouter = createBrowserRouter([{
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: '/',
+        element: <MainContainer />
+      },
+      {
+        path: '/watch',
+        element: <WatchPage />
+      }
+    ]
+  },
+
+  ]);
+
+
   return (
     <div className="">
       <Provider store={store}>
         <Head />
-        <Body />
+        <RouterProvider router={appRouter} />
       </Provider>
     </div>
   );
