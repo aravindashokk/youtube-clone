862482f58aed5901245ee1595e809107d0156e97
aravindashokk <aravindkayal@gmail.com>
2025-01-26 18:28:31 -0600

    feat(ui): embed video in watchpage and searchParams based on id

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { closeMenu } from '../utilities/appSlice';
+import { useSearchParams } from 'react-router-dom';
 
 const WatchPage = () => {
+    const [searchParams] = useSearchParams();
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -10,8 +12,8 @@ const WatchPage = () => {
     }, []);
 
     return (
-        <div>
-            WatchPage
+        <div className='px-5'>
+            <iframe width="1000" height="500" src={"https://www.youtube.com/embed/"+searchParams.get("v")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
     )
 }
