ab4df28d533d2cdb7607e4ff6a3cd4ce284b58d5
aravindashokk <aravindkayal@gmail.com>
2025-01-27 21:08:36 -0600

    feat(ui): search bar functionality with css

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import { HAMBURGER_ICON, USER_ICON, YOUTUBE_ICON } from '../utilities/constants';
+import React, { useState, useEffect } from 'react'
+import { HAMBURGER_ICON, USER_ICON, YOUTUBE_ICON, YOUTUBE_SEARCH_API } from '../utilities/constants';
 import { useDispatch } from 'react-redux';
 import { toggleMenu } from '../utilities/appSlice';
 
 const Head = () => {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
+    const [showSuggestions, setShowSuggestions] = useState(false);
     const dispatch = useDispatch();
+
+    //make api call after 200ms - debouncing
+    useEffect(() => {
+        const timer = setTimeout(() => getSearchSuggestions(), 200);
+        return () => {
+            clearTimeout(timer);
+        }
+
+    }, [searchQuery]);
+
+    const getSearchSuggestions = async () => {
+        const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+        const json = await data.json();
+        // console.log(json[1]);
+        setSuggestions(json[1]);
+    };
+
     const toggleMenuHandler = () => {
         dispatch(toggleMenu());
     };
@@ -18,8 +38,17 @@ const Head = () => {
                 </a>
             </div>
             <div className='col-span-10 px-10'>
-                <input className='w-1/2 border border-gray-400 p-2 rounded-l-full' type='text' />
-                <button className='border border-gray-400 px-5 py-2 mt-1 rounded-r-full bg-gray-100'>Search</button>
+                <div>
+                    <input className='px-5 w-1/2 border border-gray-400 p-2 rounded-l-full' type='text' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onFocus={()=>setShowSuggestions(true)} onBlur={()=>setShowSuggestions(false)} />
+                    <button className='border border-gray-400 px-5 py-2 mt-1 rounded-r-full bg-gray-100'>Search</button>
+                </div>
+                {showSuggestions && (<div className='fixed bg-white py-2 px-2 w-[32rem] shadow-lg rounded-lg border-gray-100'>
+                    <ul>
+                        {suggestions.map((suggestion) => (
+                            <li className='py-2 px-3 shadow-sm hover:bg-gray-100'>{suggestion}</li>
+                        ))}
+                    </ul>
+                </div>)}
             </div>
             <div className='col-span-1'>
                 <img className='h-12' alt="user" src={USER_ICON} />
--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -2,5 +2,5 @@ export const HAMBURGER_ICON = "https://static.vecteezy.com/system/resources/prev
 export const YOUTUBE_ICON = "https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo-2017-present.jpg";
 export const USER_ICON = "https://static.vecteezy.com/system/resources/previews/000/550/731/original/user-icon-vector.jpg";
 export const BUTTON_LIST_SUGGESTION = ["All", "Gaming", "Songs", "Live", "Cricket", "Chess", "News", "Cooking", "Podcasts", "Music", "History", "Mixes", "Football"];
-export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&maxResults=50&key="+process.env.REACT_APP_MY_GOOGLE_API_KEY;
-
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&maxResults=50&key=" + process.env.REACT_APP_MY_GOOGLE_API_KEY;
+export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
