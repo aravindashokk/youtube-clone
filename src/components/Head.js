366c05a9e2871ddedf636dcae8ec6a484f7e82e0
aravindashokk <aravindkayal@gmail.com>
2025-01-28 14:00:18 -0600

    feat(ui): Update Search results with debouncing and caching to reduce uneccesary API calls.

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import { HAMBURGER_ICON, USER_ICON, YOUTUBE_ICON, YOUTUBE_SEARCH_API } from '../utilities/constants';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggleMenu } from '../utilities/appSlice';
+import { cacheResults } from '../utilities/searchSlice';
 
 const Head = () => {
     const [searchQuery, setSearchQuery] = useState("");
     const [suggestions, setSuggestions] = useState([]);
     const [showSuggestions, setShowSuggestions] = useState(false);
+    const searchCache = useSelector((store => store.search)); 
     const dispatch = useDispatch();
 
     //make api call after 200ms - debouncing
     useEffect(() => {
-        const timer = setTimeout(() => getSearchSuggestions(), 200);
+        const timer = setTimeout(() => {
+            if(searchCache[searchQuery]){
+                setSuggestions(searchCache[searchQuery]);
+            }else{
+                getSearchSuggestions()
+            }
+            
+        }, 200);
+
         return () => {
             clearTimeout(timer);
         }
@@ -23,6 +33,11 @@ const Head = () => {
         const json = await data.json();
         // console.log(json[1]);
         setSuggestions(json[1]);
+        
+        //update cache
+        dispatch(cacheResults({
+            [searchQuery]: json[1],
+        }));
     };
 
     const toggleMenuHandler = () => {
@@ -39,7 +54,7 @@ const Head = () => {
             </div>
             <div className='col-span-10 px-10'>
                 <div>
-                    <input className='px-5 w-1/2 border border-gray-400 p-2 rounded-l-full' type='text' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onFocus={()=>setShowSuggestions(true)} onBlur={()=>setShowSuggestions(false)} />
+                    <input className='px-5 w-1/2 border border-gray-400 p-2 rounded-l-full' type='text' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onFocus={() => setShowSuggestions(true)} onBlur={() => setShowSuggestions(false)} />
                     <button className='border border-gray-400 px-5 py-2 mt-1 rounded-r-full bg-gray-100'>Search</button>
                 </div>
                 {showSuggestions && (<div className='fixed bg-white py-2 px-2 w-[32rem] shadow-lg rounded-lg border-gray-100'>
